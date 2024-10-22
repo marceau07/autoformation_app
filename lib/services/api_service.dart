09eee6fd16ad74f4ae13738cceb136a396d6e349
marceau07 <marceau0707@gmail.com>
2024-10-22 17:09:05 +0200

    Avertissement du statut du quiz

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -12,13 +12,34 @@ class ApiService {
       print("QUERYING API: " + _baseUrlApi + '/' + sUuid);
       final response = await http.get(Uri.parse(_baseUrlApi + '/' + sUuid));
 
+      print("RESPONSE: " + response.statusCode.toString());
       if (response.statusCode == 200) {
         List<dynamic> jsonData = jsonDecode(response.body);
         print(jsonData);
         jsonData.map((item) => print(item));
         return jsonData.map((item) => Question.fromJson(item)).toList();
       } else if (response.statusCode == 404) {
-        throw Exception('Quiz non trouvé');
+        // Si la réponse est une liste de JSON
+        List<dynamic> jsonData = jsonDecode(response.body);
+
+        // Assurer que la liste n'est pas vide et qu'elle contient au moins un objet
+        if (jsonData.isNotEmpty && jsonData[0] is Map<String, dynamic>) {
+          print("C'est pas vide !!");
+          Map<String, dynamic> firstItem = jsonData[0];
+
+          // Vérifier si le message est présent dans le premier objet de la liste
+          if (firstItem.containsKey('message')) {
+            print(firstItem['message']);
+          } else {
+            print('Aucun message d\'erreur disponible');
+          }
+
+          // Lancer une exception avec le message d'erreur
+          throw Exception(
+              firstItem['message'] ?? 'Erreur 404 : Ressource non trouvée');
+        } else {
+          throw Exception('Réponse inattendue du serveur');
+        }
       } else {
         throw Exception('Erreur lors de la récupération des questions');
       }
